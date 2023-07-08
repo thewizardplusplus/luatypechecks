0882609999f5e0cdbb0b5c413d895d3bfd7d4243
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-07-09 02:09:55 +0600

    Add the `is_number_or_nil()` function

--- checks.lua
+++ checks.lua
@@ -3,11 +3,22 @@
 
 local checks = {}
 
+local function _is_nil(value)
+  return type(value) == "nil"
+end
+
 ---
 -- @tparam any value
 -- @treturn bool
 function checks.is_number(value)
   return type(value) == "number"
+end
+
+---
+-- @tparam any value
+-- @treturn bool
+function checks.is_number_or_nil(value)
+  return checks.is_number(value) or _is_nil(value)
 end
 
 return checks
--- checks_test.lua
+++ checks_test.lua
@@ -49,3 +49,49 @@ for _, data in ipairs({
     data.want(result)
   end
 end
+
+-- checks.is_number_or_nil()
+for _, data in ipairs({
+  {
+    name = "test_is_number_or_nil/nil",
+    args = { value = nil },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_number_or_nil/boolean",
+    args = { value = true },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_number_or_nil/number/integer",
+    args = { value = 23 },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_number_or_nil/number/float",
+    args = { value = 2.3 },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_number_or_nil/string",
+    args = { value = "test" },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_number_or_nil/function",
+    args = { value = function() end },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_number_or_nil/table",
+    args = { value = {} },
+    want = luaunit.assert_false,
+  },
+}) do
+  TestChecks[data.name] = function()
+    local result = checks.is_number_or_nil(data.args.value)
+
+    luaunit.assert_is_boolean(result)
+    data.want(result)
+  end
+end
