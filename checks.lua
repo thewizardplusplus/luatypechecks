bab55a9648ca2b247c8987f396c75fd8e8f00839
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-07-09 10:57:19 +0600

    Add the `is_table()` function

--- checks.lua
+++ checks.lua
@@ -63,4 +63,44 @@ function checks.is_function_or_nil(value)
   return checks.is_function(value) or _is_nil(value)
 end
 
+---
+-- @tparam any value
+-- @tparam[opt] func key_checker func(value: any): bool
+-- @tparam[optchain] func value_checker func(value: any): bool
+-- @treturn bool
+function checks.is_table(value, key_checker, value_checker)
+  assert(checks.is_function_or_nil(key_checker))
+  assert(checks.is_function_or_nil(value_checker))
+
+  if type(value) ~= "table" then
+    return false
+  end
+  if key_checker == nil and value_checker == nil then
+    return true
+  end
+
+  for key, value in pairs(value) do
+    if key_checker ~= nil and not key_checker(key) then
+      return false
+    end
+    if value_checker ~= nil and not value_checker(value) then
+      return false
+    end
+  end
+
+  return true
+end
+
+---
+-- @tparam any value
+-- @tparam[opt] func key_checker func(value: any): bool
+-- @tparam[optchain] func value_checker func(value: any): bool
+-- @treturn bool
+function checks.is_table_or_nil(value, key_checker, value_checker)
+  assert(checks.is_function_or_nil(key_checker))
+  assert(checks.is_function_or_nil(value_checker))
+
+  return checks.is_table(value, key_checker, value_checker) or _is_nil(value)
+end
+
 return checks
--- checks_test.lua
+++ checks_test.lua
@@ -371,3 +371,221 @@ for _, data in ipairs({
     data.want(result)
   end
 end
+
+-- checks.is_table()
+for _, data in ipairs({
+  {
+    name = "test_is_table/nil",
+    args = { value = nil },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/boolean",
+    args = { value = true },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/number/integer",
+    args = { value = 23 },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/number/float",
+    args = { value = 2.3 },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/string",
+    args = { value = "test" },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/function",
+    args = { value = function() end },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/table",
+    args = { value = {} },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table/table/key_checker/true",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_string,
+    },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table/table/key_checker/false",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_number,
+    },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/table/value_checker/true",
+    args = {
+      value = { one = 1, two = 2 },
+      value_checker = checks.is_number,
+    },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table/table/value_checker/false",
+    args = {
+      value = { one = 1, two = 2 },
+      value_checker = checks.is_string,
+    },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/table/key_and_value_checkers/true",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_string,
+      value_checker = checks.is_number,
+    },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table/table/key_and_value_checkers/key_checker_false",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_boolean,
+      value_checker = checks.is_number,
+    },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table/table/key_and_value_checkers/value_checker_false",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_string,
+      value_checker = checks.is_boolean,
+    },
+    want = luaunit.assert_false,
+  },
+}) do
+  TestChecks[data.name] = function()
+    local result = checks.is_table(
+      data.args.value,
+      data.args.key_checker,
+      data.args.value_checker
+    )
+
+    luaunit.assert_is_boolean(result)
+    data.want(result)
+  end
+end
+
+-- checks.is_table_or_nil()
+for _, data in ipairs({
+  {
+    name = "test_is_table_or_nil/nil",
+    args = { value = nil },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table_or_nil/boolean",
+    args = { value = true },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/number/integer",
+    args = { value = 23 },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/number/float",
+    args = { value = 2.3 },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/string",
+    args = { value = "test" },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/function",
+    args = { value = function() end },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/table",
+    args = { value = {} },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table_or_nil/table/key_checker/true",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_string,
+    },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table_or_nil/table/key_checker/false",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_number,
+    },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/table/value_checker/true",
+    args = {
+      value = { one = 1, two = 2 },
+      value_checker = checks.is_number,
+    },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table_or_nil/table/value_checker/false",
+    args = {
+      value = { one = 1, two = 2 },
+      value_checker = checks.is_string,
+    },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/table/key_and_value_checkers/true",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_string,
+      value_checker = checks.is_number,
+    },
+    want = luaunit.assert_true,
+  },
+  {
+    name = "test_is_table_or_nil/table/key_and_value_checkers/key_checker_false",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_boolean,
+      value_checker = checks.is_number,
+    },
+    want = luaunit.assert_false,
+  },
+  {
+    name = "test_is_table_or_nil/table/key_and_value_checkers/value_checker_false",
+    args = {
+      value = { one = 1, two = 2 },
+      key_checker = checks.is_string,
+      value_checker = checks.is_boolean,
+    },
+    want = luaunit.assert_false,
+  },
+}) do
+  TestChecks[data.name] = function()
+    local result = checks.is_table_or_nil(
+      data.args.value,
+      data.args.key_checker,
+      data.args.value_checker
+    )
+
+    luaunit.assert_is_boolean(result)
+    data.want(result)
+  end
+end
